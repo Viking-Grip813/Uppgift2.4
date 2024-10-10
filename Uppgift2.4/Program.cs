45c586b15f18d6797a494fba8003b823692aab94
vist07001 <vist07001@E-PW05CGR8>
2024-10-10 12:34:51 +0200

    Add project files.

---
+++ Uppgift2.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift2_4
+{ class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Johans lön:");
+            string Johanslön = Console.ReadLine();
+            int tal = int.Parse(Johanslön);
+            Console.WriteLine("Wiktors lön:");
+            string Wiktorslön = Console.ReadLine();
+            int tal2 = int.Parse(Wiktorslön);
+            Console.WriteLine("Emils lön:");
+            string Emilslön = Console.ReadLine();
+            int tal3 = int.Parse(Emilslön);
+            int medel_lön = (tal + tal2 + tal3)/3;
+            Console.WriteLine("Medellönen är " + medel_lön);
+        }
+    }
+}
